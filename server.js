ddb813684b1e478ac972aa9c27a40827c41a3823
dannyvassallo <danielvassallo87@gmail.com>
2016-10-10 10:38:22 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+var fs = require('fs'),
+nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport(process.env.EMAIL_CREDS);
+
+var recipients = fs.readFileSync('recipients.txt').toString().split("\n");
+
+var emailTemplate = fs.createReadStream('template.html');
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: process.env.FROM, // sender address
+    to: recipients.toString(), // list of receivers
+    subject: process.env.SUBJECT, // Subject line
+    html: emailTemplate
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+});
